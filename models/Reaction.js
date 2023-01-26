bd2ad105c0ebc6bb15a4f0925b9f3ff013b7a73b
BillStephens2022 <two4onebill@yahoo.com>
2023-01-26 09:07:38 -0500

    set up Reaction schema

---
+++ models/Reaction.js
@@ -0,0 +1,33 @@
+const { Schema } = require('mongoose');
+
+// Schema to create Thought model
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId()
+    },
+    reactionBody: { 
+        type: String,
+        required: true,
+        maxlength: 280
+    },
+    username: { 
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    }
+  },
+  {
+    // Indicates that virtuals to be included with our response
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+module.exports = reactionSchema;
